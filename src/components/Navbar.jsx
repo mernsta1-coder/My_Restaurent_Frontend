1076213dd0a066ede686500d733c16ae87318ecc
Yugam Gupta <mern.sta1@gmail.com>
2026-02-03 17:46:15 +0530

    next changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = ({ cartCount }) => {
     }
 
     try {
-      const res = await axiosWithToken().get("/profile");
+      const res = await axiosWithToken().get("/api/users/profile");
       setUser(res.data.user);
     } catch (err) {
       console.error("Profile fetch failed:", err.message);
